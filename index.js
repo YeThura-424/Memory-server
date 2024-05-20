8d680395586f1c56a66e8b175bcf644bbede37f1
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-20 21:52:39 +0630

    mongoose setup

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// mongoose url 
+const CONNECTION_URL = 'mongodb+srv://yairtr74:completeblog@cluster0.znd5coc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const PORT = process.env.PORT || 5000;
+
+//mongo setup
+mongoose.connection(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is Running on port : ${PORT}`));
+    }).catch((error) => {
+        console.log(error);
+    });
+mongoose.set('useFindAndModify', false);
