147085c82f33654f36436f5ae8eb7432436053df
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-20 22:30:22 +0630

    routing

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+//routes
+import postRoute from './routes/posts.js';
 const app = express();
+
+//routers declarization
+app.use('/posts', postRoute); // first param is prefix for post route and sceond is the post route;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,10 +20,11 @@ const CONNECTION_URL = 'mongodb+srv://yairtr74:completeblog@cluster0.znd5coc.mon
 const PORT = process.env.PORT || 5000;
 
 //mongo setup
-mongoose.connection(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => console.log(`Server is Running on port : ${PORT}`));
     }).catch((error) => {
         console.log(error);
     });
-mongoose.set('useFindAndModify', false);
+
+// mongoose.set('useFindAndModify', false);
